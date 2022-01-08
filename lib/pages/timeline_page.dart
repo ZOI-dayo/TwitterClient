8ce71d362a8cb770d526e7f1dd40f53b8d65825b
masafumi terazono <masafumi_t@0x48lab.com>
2022-01-08 18:17:59 +0900

    PullToRefreshを追加

--- lib/pages/timeline_page.dart
+++ lib/pages/timeline_page.dart
@@ -4,65 +4,59 @@ import 'package:provider/src/provider.dart';
 import 'package:twitter_test/pages/timeline_model.dart';
 
 class TimelinePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Scaffold(
-            body: Scrollbar(
-      isAlwaysShown: false,
-      child: SingleChildScrollView(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        ElevatedButton(
-          onPressed: () => context.read<TimelineModel>().getTimeline(context),
-          child: Text('reload'),
-        ),
-        // Text(
-        //     "${context.watch<TimelineModel>().apiResponse}\n\n${DateTime.now()}"),
-        SizedBox(
-          height: 700,
-          width: 500,
-          child: ListView.builder(
-            itemCount: context.watch<TimelineModel>().tweets.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.red),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                // color: Color.fromARGB(255, 255, 255, 0),
-                child: Row(children: [
-                  // Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
-                  Image.network(context
-                      .watch<TimelineModel>()
-                      .tweets
-                      .elementAt(index)
-                      .user
-                      .profile_image_url_https),
-                  Flexible(
-                      child: Column(
-                    children: [
-                      Text(
-                          context
+      body: Column(
+        children: [
+          Expanded(
+            child: RefreshIndicator(
+                onRefresh: () async { context.read<TimelineModel>().getTimeline(context); },
+                  child: ListView.builder(
+                    itemCount: context.watch<TimelineModel>().tweets.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.red),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        // color: Color.fromARGB(255, 255, 255, 0),
+                        child: Row(children: [
+                          // Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+                          Image.network(context
                               .watch<TimelineModel>()
                               .tweets
                               .elementAt(index)
                               .user
-                              .name,
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      context
-                          .watch<TimelineModel>()
-                          .tweets
-                          .elementAt(index)
-                          .getTweetContent()
-                    ],
-                  ))
-                ]),
-              );
-            },
+                              .profile_image_url_https),
+                          Flexible(
+                              child: Column(
+                            children: [
+                              Text(
+                                  context
+                                      .watch<TimelineModel>()
+                                      .tweets
+                                      .elementAt(index)
+                                      .user
+                                      .name,
+                                  style: TextStyle(fontWeight: FontWeight.bold)),
+                              context
+                                  .watch<TimelineModel>()
+                                  .tweets
+                                  .elementAt(index)
+                                  .getTweetContent()
+                            ],
+                          ))
+                        ]),
+                      );
+                    },
+                  ),
+                ),
           ),
-        ),
-      ])),
-    )));
+        ],
+      ),
+    );
   }
 }
