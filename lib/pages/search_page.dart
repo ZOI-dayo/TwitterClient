207b724016c352bf5c122d77c7c5af952c8a322d
masafumi terazono <hijirinhijirin@gmail.com>
2022-03-19 18:32:27 +0900

    検索ボックス追加

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -22,8 +22,37 @@ class _SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Column(
-        children: [Text('search')],
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SearchTextField(),
+          ),
+          Text('search')],
       ),
     );
   }
+}
+
+class SearchTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey,
+                ),
+              ),
+              hintText: 'Enter a search term'
+            ),
+          ),
+        ),
+        IconButton(onPressed: (){}, icon: Image.asset('assets/icons/ic_send.png'))
+      ],
+    );
+  }
+  
 }
