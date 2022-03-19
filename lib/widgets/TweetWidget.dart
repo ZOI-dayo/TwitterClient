a3bb38ef305d7f4b07ca7fc4545c9398207244f5
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-19 16:57:49 +0900

    RT、ブックマークボタン実装・数字表示(仮) #5

--- lib/widgets/TweetWidget.dart
+++ lib/widgets/TweetWidget.dart
@@ -9,10 +9,21 @@ import '../twitter_api.dart';
 class TweetWidget extends StatelessWidget {
   HomeModel model;
   Tweet tweet;
+
   TweetWidget(this.model, this.tweet) : super();
 
   static Future<Color> getLikeColor(Tweet tweet) async {
     return await TwitterAPI().isLiked(tweet.id_str) ? Colors.red : Colors.white;
+  }
+
+  static Future<Color> getRetweetColor(Tweet tweet) async {
+    // return await TwitterAPI().isLiked(tweet.id_str) ? Colors.red : Colors.white;
+    return Colors.white;
+  }
+
+  static Future<Color> getBookmarkColor(Tweet tweet) async {
+    // return await TwitterAPI().isLiked(tweet.id_str) ? Colors.red : Colors.white;
+    return Colors.white;
   }
 
   @override
@@ -23,68 +34,126 @@ class TweetWidget extends StatelessWidget {
         border: Border.all(color: Colors.red),
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Row(children: [
-        Image.network(tweet.user.profile_image_url_https),
-        Flexible(
+      child: Row(
+        children: [
+          Image.network(tweet.user.profile_image_url_https),
+          Flexible(
             child: Column(
               children: [
                 Text(tweet.user.name,
                     style: TextStyle(fontWeight: FontWeight.bold)),
                 tweet.getTweetContent(context),
                 SizedBox(
-                    child: Row(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.comment),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.transparent,
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            TwitterAPI().like(tweet.id_str);
-                            model.refresh();
+                  child: Row(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Consumer<HomeModel>(
+                          builder: (_, __, ___) {
+                            return Row(
+                              children: [
+                                Icon(Icons.comment, color: Colors.white),
+                                Text("10"),
+                              ],
+                            );
                           },
-                          child: Consumer<HomeModel>(
-                            builder: (_, __, ___) {
-                              return FutureBuilder(
-                                future: TweetWidget.getLikeColor(tweet),
-                                builder: (BuildContext context,
-                                    AsyncSnapshot<Color> snapshot) {
-                                  if (snapshot.hasData) {
-                                    return Icon(Icons.favorite,
-                                        color: snapshot.data);
-                                  } else {
-                                    return Icon(Icons.favorite);
-                                  }
-                                },
-                              );
-                            },
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.transparent,
-                          ),
                         ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.loop),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.transparent,
-                          ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
                         ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.bookmark_border),
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.transparent,
-                          ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          TwitterAPI().like(tweet.id_str);
+                          model.refresh();
+                        },
+                        child: Consumer<HomeModel>(
+                          builder: (_, __, ___) {
+                            return Row(
+                              children: [
+                                FutureBuilder(
+                                  future: TweetWidget.getLikeColor(tweet),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<Color> snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Icon(Icons.favorite,
+                                          color: snapshot.data);
+                                    } else {
+                                      return Icon(Icons.favorite);
+                                    }
+                                  },
+                                ),
+                                Text("10"),
+                              ],
+                            );
+                          },
                         ),
-                      ],
-                    ))
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Consumer<HomeModel>(
+                          builder: (_, __, ___) {
+                            return Row(
+                              children: [
+                                FutureBuilder(
+                                  future: TweetWidget.getRetweetColor(tweet),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<Color> snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Icon(Icons.loop,
+                                          color: snapshot.data);
+                                    } else {
+                                      return Icon(Icons.loop);
+                                    }
+                                  },
+                                ),
+                                Text("10"),
+                              ],
+                            );
+                          },
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Consumer<HomeModel>(
+                          builder: (_, __, ___) {
+                            return Row(
+                              children: [
+                                FutureBuilder(
+                                  future: TweetWidget.getBookmarkColor(tweet),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<Color> snapshot) {
+                                    if (snapshot.hasData) {
+                                      return Icon(Icons.bookmark_border,
+                                          color: snapshot.data);
+                                    } else {
+                                      return Icon(Icons.bookmark_border);
+                                    }
+                                  },
+                                ),
+                                Text("10"),
+                              ],
+                            );
+                          },
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.transparent,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
-            ))
-      ]),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
